8adad158442d082c65dcec18a1759f5787af197f
mihierkansal <mihierkansal@outlook.com>
2025-04-05 17:14:50 -0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,83 @@
+import { Accessor, createMemo, createSignal, For } from "solid-js";
+
+function App() {
+  const hour = createSignal(new Date().getHours());
+  const minute = createSignal(new Date().getMinutes());
+  const second = createSignal(new Date().getSeconds());
+
+  setInterval(() => {
+    const date = new Date();
+    hour[1](date.getHours() > 12 ? date.getHours() - 12 : date.getHours());
+    minute[1](date.getMinutes());
+    second[1](date.getSeconds());
+  }, 1000);
+
+  return (
+    <>
+      <div class="clock metal linear">
+        <div class="logo">BABOOLA</div>
+        <div class="rollers-cnt">
+          <div style={{ display: "flex" }}>
+            <RollingNumbersDisplay len={2} value={hour[0]} />
+            :
+            <RollingNumbersDisplay len={2} value={minute[0]} />
+            :
+            <RollingNumbersDisplay len={2} value={second[0]} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+function RollingNumbersDisplay(props: {
+  len: number;
+  value: Accessor<number>;
+}) {
+  return (
+    <>
+      <For each={new Array(props.len).fill(0)}>
+        {(_, i) => {
+          const v = createMemo(() => {
+            return Number(
+              props.value().toString().padStart(props.len, "0")[i()]
+            );
+          });
+          return (
+            <>
+              <RollingNumberDisplay value={v} />
+            </>
+          );
+        }}
+      </For>
+    </>
+  );
+}
+function RollingNumberDisplay(props: { value: Accessor<number> }) {
+  const FONT_SIZE = 50;
+  return (
+    <>
+      <div class="rolling-display">
+        <div
+          class="numbers-cnt"
+          style={{
+            display: "flex",
+            "flex-direction": "column",
+            "margin-top": `-${FONT_SIZE * props.value()}px`,
+          }}
+        >
+          <For each={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]}>
+            {(item) => {
+              return (
+                <div class="number-cnt">
+                  <div class="number">{item}</div>
+                </div>
+              );
+            }}
+          </For>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
